29e8fa4756eddd5e2c12267950dc36fc9da8f206
tunguyenarea <tunguyenarea@gmail.com>
2025-02-27 21:26:00 +0700

    Commit 3

--- components/custom/edit-form.tsx
+++ components/custom/edit-form.tsx
@@ -36,7 +36,7 @@ export default function EditForm({ note_id, title, author_id, content }: { note_
         <textarea name="content" defaultValue={content} placeholder="Content..." className="rounded-lg w-full h-64 md:h-96 border-2 p-2" required></textarea>
       </div>
       <div className="grid gap-6 md:flex md:justify-around my-6 md:my-12">
-        <Link href="/home">
+        <Link href={`/home/${note_id}`}>
           <button className="bg-black dark:bg-white rounded-lg text-white dark:text-black p-2 w-full md:w-36 h-10 hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 hover:text-white">Cancel</button>
         </Link>
         <Submit />
