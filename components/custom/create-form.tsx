567509abbd4f9704d55895ea682b88d659bd7deb
tunguyenarea <tunguyenarea@gmail.com>
2025-03-08 10:23:26 +0700

    Commit 3

--- components/custom/create-form.tsx
+++ components/custom/create-form.tsx
@@ -34,7 +34,7 @@ export default function CreateForm({ author_id }: { author_id: string | undefine
         <label className="text-xl bg-gradient-to-r from-indigo-500 to-pink-500 bg-clip-text font-bold text-transparent">Content</label>
         <textarea name="content" placeholder="Content..." className="rounded-lg w-full h-64 md:h-96 border-2 p-2" required></textarea>
       </div>
-      <div className="grid gap-6 md:flex md:justify-around my-6 md:my-12">
+      <div className="grid gap-6 md:flex md:justify-between my-6 md:my-12">
         <Link href="/home">
           <button className="bg-black dark:bg-white rounded-lg text-white dark:text-black p-2 w-full md:w-36 h-10 hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 hover:text-white">Cancel</button>
         </Link>
