b9fd107fdb34a09143f5a99d457e94ae6f1b6ade
tunguyenarea <tunguyenarea@gmail.com>
2025-03-08 09:44:40 +0700

    Commit 3

--- app/home/[id]/page.tsx
+++ app/home/[id]/page.tsx
@@ -12,7 +12,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
   <main>
     <SectionTwo id={id}/>
 
-    <div className="grid gap-6 md:flex md:justify-around m-6 md:m-12">
+    <div className="grid gap-6 md:flex md:justify-between m-6 md:m-8">
       <Link href="/home">
         <button className="bg-black dark:bg-white rounded-lg text-white dark:text-black p-2 w-full md:w-36 h-10 hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 hover:text-white">Back</button>
       </Link>
--- components/custom/edit-form.tsx
+++ components/custom/edit-form.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { editNote, State } from '@/app/lib/actions';
+import { Note } from '@/app/lib/definitions';
 import { useActionState } from 'react';
 import { useFormStatus } from 'react-dom';
 import Link from 'next/link';
@@ -14,9 +15,9 @@ function Submit() {
   );
 }
 
-export default function EditForm({ note_id, title, author_id, content }: { note_id: string, title: string, author_id: string, content: string }) {
+export default function EditForm({ note }: { note: Note }) {
   const initialState: State = { message: null, errors: {} };
-  const editNoteId = editNote.bind(null, note_id );
+  const editNoteId = editNote.bind(null, note.note_id );
   const [state, formAction] = useActionState(editNoteId, initialState);
 
   return (
@@ -26,17 +27,17 @@ export default function EditForm({ note_id, title, author_id, content }: { note_
     <form action={formAction}>
       <div className="my-6">
         <label className="text-xl bg-gradient-to-r from-indigo-500 to-pink-500 bg-clip-text font-bold text-transparent">Title</label>
-          <input name="title" defaultValue={title} placeholder="Title..." className="rounded-lg w-full border-2 p-2" required></input>
+          <input name="title" defaultValue={note.title} placeholder="Title..." className="rounded-lg w-full border-2 p-2" required></input>
       </div>
       <div>
-        <input name="author_id" defaultValue={author_id} type="hidden" className=""></input>
+        <input name="author_id" defaultValue={note.author_id} type="hidden" className=""></input>
       </div>
       <div className="my-6">
         <label className="text-xl bg-gradient-to-r from-indigo-500 to-pink-500 bg-clip-text font-bold text-transparent">Content</label>
-        <textarea name="content" defaultValue={content} placeholder="Content..." className="rounded-lg w-full h-64 md:h-96 border-2 p-2" required></textarea>
+        <textarea name="content" defaultValue={note.content} placeholder="Content..." className="rounded-lg w-full h-64 md:h-96 border-2 p-2" required></textarea>
       </div>
-      <div className="grid gap-6 md:flex md:justify-around my-6 md:my-12">
-        <Link href={`/home/${note_id}`}>
+      <div className="grid gap-6 md:flex md:justify-between my-6 md:my-12">
+        <Link href={`/home/${note.note_id}`}>
           <button className="bg-black dark:bg-white rounded-lg text-white dark:text-black p-2 w-full md:w-36 h-10 hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 hover:text-white">Cancel</button>
         </Link>
         <Submit />
